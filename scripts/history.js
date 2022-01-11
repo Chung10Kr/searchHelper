8f4fd52872be8e655560b8eadde558e8d1e13de1
Chung10Kr <coohg93@hanmail.net>
2022-01-11 23:24:07 +0900

    [2022-01-11][crlee][BR-6] - 이력 List 생성 - 이력 삭제 기능 추가 - 이력 내용 정리(일자, 타입, 내용, URL)

--- scripts/history.js
+++ scripts/history.js
@@ -8,14 +8,46 @@ class historyJs
 
     async initControl()
     {
-        let history = await chrome.storage.sync.get("history");
-        history = history["history"] || [];
-        console.log(history)
-
+        await this.getHistory();
+        this.renderHistory();
     }
     initEvent()
     {
-        
+        let self = this;
+        $(document).on("click",".move",function(e){
+            let l = $(this).attr('data-key');
+            chrome.tabs.create({ url: l, active: true }); 
+        });
+        $("#initHistory").click(async function(e){
+            await self.initHistory();
+            await self.getHistory();
+            self.renderHistory();
+        })
+    }
+    async getHistory()
+    {
+        let history = await chrome.storage.sync.get("history");
+        if( JSON.stringify(history) == "{}" ) return false;
+        this.history = history['history'];
+    }
+    renderHistory()
+    {
+        let self = this;
+        let history = this.history;
+        $("#historyList").empty();
+        for( let key in history ){
+            let data = history[key];
+            let str = `<tr>
+                        <td><input type="checkbox"></td>
+                        <td>${data['create_date']}</td>
+                        <td class="move" data-key="${data['url']}" style="cursor: pointer;">${data['searchType']} - >${data['txt']}</a></td>
+                       </tr>`;
+            $("#historyList").append(str);
+        };
+    }
+    async initHistory()
+    {
+         await chrome.storage.sync.clear();
     }
 };
 
--- scripts/popup.js
+++ scripts/popup.js
@@ -8,6 +8,7 @@ class popupJs
 
     initControl()
     {
+
         this.searchTxt = document.getElementById("searchTxt");
         this.searchBtn = document.getElementById("searchBtn");
         this.searchTxt.focus();
@@ -17,35 +18,42 @@ class popupJs
         let self = this;
         document.getElementById('searchByGoogle').onclick = () => { 
             self.url = (self.searchTxt.value === '') ? 'https://google.com' : `https://www.google.com/search?q=${self.searchTxt.value}`;
+            self.searchType = "google";
             self.search();
         };
         document.getElementById('searchBygitHub').onclick = () => { 
             self.url = (self.searchTxt.value === '') ? 'https://github.com' : `https://github.com/search?q=${self.searchTxt.value}`;
+            self.searchType = "github";
             self.search();
         };
         
         document.getElementById('searchByYoutube').onclick = () => {
             self.url = (self.searchTxt.value === '') ? 'https://www.youtube.com' : `https://www.youtube.com//results?search_query=${self.searchTxt.value}`;
+            self.searchType = "youtube";
             self.search();
         };
         
         document.getElementById('searchByNaver').onclick = () => {
             self.url = (self.searchTxt.value === '') ? 'https://www.naver.com' : `https://search.naver.com/search.naver?query=${self.searchTxt.value}`;
+            self.searchType = "naver";
             self.search();
         };
         
         document.getElementById('searchByDaum').onclick = () => {
             self.url = (self.searchTxt.value === '') ? 'https://www.daum.com' : `https://search.daum.net/search?w=tot&q=${self.searchTxt.value}`;
+            self.searchType = "daum";
             self.search();
         };
         document.getElementById('searchByYahoo').onclick = () => {
             self.url = (self.searchTxt.value === '') ? 'https://search.yahoo.com' : `https://search.yahoo.com/search?p=${self.searchTxt.value}`;
+            self.searchType = "yahoo";
             self.search();
         };
         document.getElementById('searchTxt').onkeyup = (e) => {
             //Enter
             if(e.keyCode == 13){
                 self.url = (self.searchTxt.value === '') ? 'https://google.com' : `https://www.google.com/search?q=${self.searchTxt.value}`;
+                self.searchType = "google";
                 self.search();
             };
         };
@@ -53,21 +61,46 @@ class popupJs
     async search()
     {
         let l = this.url;
-        this.log();
+        await this.log();
         chrome.tabs.create({ url: l, active: true }); 
     }
     async log()
     {
         let l = this.url;
         let txt= this.searchTxt.value;
+        let searchType= this.searchType;
+        let create_date = this.now();
         let history = await chrome.storage.sync.get("history");
-        history = history['history'] || [];
-        history.push({
-                "url" : l,
-                "txt" : txt
-        });
-        chrome.storage.sync.set( {"history":history} );
+        let tmpObj = JSON.stringify(history) == "{}" ? {} : history['history'];
+        let key = this.uuidv4();
+
+        tmpObj[key] = {
+            "url" : l,
+            "txt" : txt,
+            "searchType" : searchType,
+            "create_date" : create_date
+        };
+
+        await chrome.storage.sync.set( {"history":tmpObj} );
     }
+    now()
+    {
+        var today = new Date();
+
+        var year = today.getFullYear();
+        var month = ('0' + (today.getMonth() + 1)).slice(-2);
+        var day = ('0' + today.getDate()).slice(-2);
+        
+        return `${year}-${month}-${day}`;
+    }
+    uuidv4() 
+    {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+    
 }
 
 
